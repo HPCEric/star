723260b726b3101cf778af95d20fe1ff0d994e81
saichii <overy1399@gmail.com>
2022-05-16 21:02:18 +0800

    starlux echo

---
+++ echo.php
@@ -0,0 +1,44 @@
+<?php
+$str= "STAR";
+$star = "lux";
+
+// echo $str;
+// echo "<br>";
+
+// echo "$$str";
+// echo "<br>";
+
+// echo "$str";
+// echo "<br>";
+
+// echo '$str';
+// echo "<br>";
+
+// echo "'$str'";
+// echo "<br>";
+
+// echo "str";
+// echo "<br>";
+
+// echo 'str';
+// echo "<br>";
+
+
+echo $str='$star';
+echo "<br>";
+
+echo $str="$star";
+echo "<br>";
+
+echo $str=="$star";
+echo "<br>";
+
+// echo "<br>";
+// echo 'str';
+// echo "<br>";
+// echo 'str';
+// echo "<br>";
+// echo 'str';
+// echo "<br>";
+// echo 'str';
+// echo "<br>";
